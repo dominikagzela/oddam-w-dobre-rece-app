625bde6fc827d870cbe4f93acc9dd97d59964315
Dominika Gzela <dominikagzela@gmail.com>
2022-11-18 20:41:14 +0100

    create Institution model

--- charity_donation/charity_app/models.py
+++ charity_donation/charity_app/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+INSTITUTION = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    type = models.PositiveSmallIntegerField(choices=INSTITUTION, default=1)
+    categories = models.ManyToManyField(Category)
+
