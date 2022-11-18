701334321ba2fa1e9d696aed05b6dbb5f7db4209
Dominika Gzela <dominikagzela@gmail.com>
2022-11-18 21:19:30 +0100

    create Donation model

--- charity_donation/charity_app/models.py
+++ charity_donation/charity_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 INSTITUTION = (
     (1, 'fundacja'),
@@ -17,3 +18,16 @@ class Institution(models.Model):
     type = models.PositiveSmallIntegerField(choices=INSTITUTION, default=1)
     categories = models.ManyToManyField(Category)
 
+
+class Donation(models.Model):
+    quantity = models.PositiveIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    phone_number = models.PositiveIntegerField()
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default='', on_delete=models.CASCADE)
